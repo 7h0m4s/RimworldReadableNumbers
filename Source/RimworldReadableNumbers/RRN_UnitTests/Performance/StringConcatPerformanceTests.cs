557257630eb1636f749187a0fe721f3e3bf4a44c
7h0m4s <firestrike42@hotmail.com>
2026-06-24 23:30:58 +1000

    Create basic unit test project

---
+++ Source/RimworldReadableNumbers/RRN_UnitTests/Performance/StringConcatPerformanceTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using RimworldReadableNumbers.Patches.String;
+
+
+namespace RRN_UnitTests.Performance
+{
+
+    [TestFixture]
+    public class StringConcatPerformanceTests
+    {
+
+        [TestFixture]
+        public class Postfix
+        {
+            // Valid
+            [Test]
+            public void Postfix_Benchmark_ValidData()
+            {
+                string pattern = "${0}";
+                int numberOffset = 1000;
+                int iterations = 10000000;
+                (string __result, object[] __args)[] dataArray = new (string __result, object[] __args)[iterations];
+                for (int i = 0; i < iterations; i++)
+                {
+                    object[] args = new object[2];
+                    args[0] = pattern;
+                    args[1] = (iterations + numberOffset + 1).ToString();
+                    string result = string.Format(pattern, args[1]);
+                    dataArray[i] =  (result, args);
+                }
+                
+                var sw = Stopwatch.StartNew();
+    
+                for (int i = 0; i < iterations; i++)
+                {
+                    RimworldReadableNumbers.Patches.String.StringConcatPatch.Postfix(ref dataArray[i].__result, dataArray[i].__args);
+                }
+    
+                sw.Stop();
+                long totalTime = sw.ElapsedMilliseconds;
+                long averageTime = sw.ElapsedMilliseconds / iterations;
+
+                TestContext.WriteLine($"Total execution time ({totalTime}ms).");
+                TestContext.WriteLine($"Average execution time ({averageTime}ms) exceeded 5ms limit.");
+            }
+        }
+    }
+}
---
+++ Source/RimworldReadableNumbers/RRN_UnitTests/Utility/Validation.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace RRN_UnitTests.Utility
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+
+        [TestFixture]
+        public class IsValidNumberToConvert
+        {
+            // Valid
+            [Test]
+            public void IsValidNumberToConvert_OneThousand_ReturnsTrue()
+            {
+                string str = "1000";
+                var result = RimworldReadableNumbers.Utility.Validation.IsValidNumberToConvert(ref  str);
+                Assert.True(result.IsValid);
+                Assert.True(result.HasPeriod == false);
+                Assert.True(result.IsSigned == false);
+                Assert.True(result.PeriodIndex == -1);
+            }
+            
+            // Not Valid
+            [Test]
+            public void IsValidNumberToConvert_NullValue_ReturnFalse()
+            {
+                string testValue = null;
+                var result = RimworldReadableNumbers.Utility.Validation.IsValidNumberToConvert(ref testValue);
+                Assert.False(result.IsValid);
+            }
+            [Test]
+            public void IsValidNumberToConvert_MultiplePeriods_ReturnFalse()
+            {
+                string testValue = "1000.000.00";
+                var result = RimworldReadableNumbers.Utility.Validation.IsValidNumberToConvert(ref testValue);
+                Assert.False(result.IsValid);
+            }
+            [Test]
+            public void IsValidNumberToConvert_GreaterThan128String_ReturnFalse()
+            {
+                string testValue = new string('9', 128);;
+                var result = RimworldReadableNumbers.Utility.Validation.IsValidNumberToConvert(ref testValue);
+                Assert.False(result.IsValid);
+            }
+        }
+
+    }
+}
