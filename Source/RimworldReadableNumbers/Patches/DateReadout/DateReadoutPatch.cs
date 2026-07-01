887495090122b3aebec97201c8ec68a4d846982e
7h0m4s <firestrike42@hotmail.com>
2026-07-01 21:49:53 +1000

    Fixed items on floor label length.

--- Source/RimworldReadableNumbers/Patches/DateReadout/DateReadoutPatch.cs
+++ Source/RimworldReadableNumbers/Patches/DateReadout/DateReadoutPatch.cs
@@ -18,15 +18,12 @@ namespace RimworldReadableNumbers.Patches.DateReadout
         {
             [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> Transpiler(
-                IEnumerable<CodeInstruction> instructions, // This contains all of the instructions in order.
-                ILGenerator ilGenerator)
+                IEnumerable<CodeInstruction> instructions, ILGenerator ilGenerator)
             {
-                MethodInfo methodToFind =
-                    AccessTools.Method(typeof(Verse.Widgets), nameof(Verse.Widgets.Label), new Type[] { typeof(Rect),  typeof(string) });
-                MethodInfo methodToCall = AccessTools.Method(typeof(Widgets.WidgetsReversePatch),
-                    nameof(Widgets.WidgetsReversePatch.OriginalLabel), new Type[] { typeof(Rect),  typeof(string) });
+                MethodInfo methodToFind = AccessTools.Method(typeof(Verse.Widgets), nameof(Verse.Widgets.Label), new Type[] { typeof(Rect),  typeof(string) });
+                MethodInfo methodToCall = AccessTools.Method(typeof(Widgets.WidgetsReversePatch), nameof(Widgets.WidgetsReversePatch.OriginalLabel), new Type[] { typeof(Rect),  typeof(string) });
                 
-                // Find every occurance of methodToFind and replace with methodToCall
+                // Find every occurrence of methodToFind and replace with methodToCall
                 return instructions.MethodReplacer(methodToFind, methodToCall).ToList();
             }
         }
---
+++ Source/RimworldReadableNumbers/Patches/GenMapUI/GenMapUIPatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using UnityEngine;
+
+namespace RimworldReadableNumbers.Patches.GenMapUI
+{
+    [HarmonyPatch(typeof(Verse.GenMapUI), nameof(Verse.GenMapUI.DrawThingLabel),new Type[] { typeof(Vector2), typeof(string), typeof(Color) })]
+    public static class GenMapUIDrawThingLabelPatch
+    {
+        
+        [HarmonyPrefix]
+        public static bool Prefix(Vector2 screenPos, ref string text, Color textColor)
+        {
+            Utility.Processing.ProcessLabel(ref text);
+            return true;
+        }
+        
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo methodToFind = AccessTools.Method(typeof(Verse.Widgets), nameof(Verse.Widgets.Label), new Type[] { typeof(Rect), typeof(string) });
+            MethodInfo methodToCall = AccessTools.Method(typeof(Widgets.WidgetsReversePatch), nameof(Widgets.WidgetsReversePatch.OriginalLabel), new Type[] { typeof(Rect), typeof(string) });
+        
+            // Find every occurrence of methodToFind and replace with methodToCall
+            return instructions.MethodReplacer(methodToFind, methodToCall).ToList();
+        }
+
+    }
+    
+    // TODO add DrawPawnLabel patch
+    // TODO add DrawText patch !!!!!!
+}
